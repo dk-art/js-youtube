c19bed0d85c1debbe295b6fdb08f6babf6927959
dk-art <rayart16@yandex.ru>
2019-04-10 16:31:50 +0300

    День 2. Домашнее задание

--- js/script.js
+++ js/script.js
@@ -80,122 +80,130 @@ switcher.addEventListener('change', () => {
    switchMode();
 });
 
+bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
+
 // Скопированный код
 const data = [
-   ['img/thumb_3.webp', 'img/thumb_4.webp', 'img/thumb_5.webp'],
-   ['#3 Верстка на flexbox CSS | Блок преимущества и галерея | Марафон верстки | Артем Исламов',
-       '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки  Урок 2',
-       '#1 Верстка реального заказа landing Page | Марафон вёрстки | Артём Исламов'],
-   ['3,6 тыс. просмотров', '4,2 тыс. просмотров', '28 тыс. просмотров'],
-   ['X9SmcY3lM-U', '7BvHoh0BrMw', 'mC8JW_aG2EM']
-];
+               [
+                  'img/thumb_3.webp', 
+                  'img/thumb_4.webp', 
+                  'img/thumb_5.webp'
+               ],
+               [
+                  '#3 Верстка на flexbox CSS | Блок преимущества и галерея | Марафон верстки | Артем Исламов',
+                  '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки  Урок 2',
+                  '#1 Верстка реального заказа landing Page | Марафон вёрстки | Артём Исламов'],
+               [
+                  '3,6 тыс. просмотров', 
+                  '4,2 тыс. просмотров', 
+                  '28 тыс. просмотров'
+               ],
+               [
+                  'X9SmcY3lM-U', 
+                  '7BvHoh0BrMw', 
+                  'mC8JW_aG2EM'
+               ]
+            ];
+
+// *******************************************************************
+
+  
 
 more.addEventListener('click', () => {
    const videosWrapper = document.querySelector('.videos__wrapper');
    more.remove();
 
-   for (let i = 0; i < data[0].length; i++) {
-      let card = document.createElement('a');
-      card.classList.add('videos__item', 'videos__item-active');
-      // card.setAtrribute('data-url', data[3][i]);
-      card.setAttribute('data-url', data[3][i]);
-      card.innerHTML = `
-         <img src="${data[0][i]}" alt="thumb">
-         <div class="videos__item-descr">
-            ${data[1][i]}
-         </div>
-         <div class="videos__item-views">
-            ${data[2][i]}
-         </div>
-      `;
-      //Помещаем карточку
-      videosWrapper.appendChild(card);
-      //запускаем скрипт с задержкой
-      setTimeout(() => {
-         card.classList.remove('videos__item-active');
-      }, 10);
-      bindNewModal(card);
+   for(let i = 0; i < data[0].length; i++) {
+       let card = document.createElement('a');
+       card.classList.add('videos__item', 'videos__item-active');
+       card.setAttribute('data-url', data[3][i]);
+       card.innerHTML = `
+           <img src="${data[0][i]}" alt="thumb">
+           <div class="videos__item-descr">
+               ${data[1][i]}
+           </div>
+           <div class="videos__item-views">
+               ${data[2][i]}
+           </div>
+       `;
+       videosWrapper.appendChild(card);
+       setTimeout(() => {
+           card.classList.remove('videos__item-active');
+       }, 10);
+       bindNewModal(card);
    }
-   
    sliceTitle('.videos__item-descr', 100);
+})
 
-});
-
-
-function sliceTitle() {
-   document.querySelectorAll(selector).forEach (item =>
-      item.textContent.trim();
-
-      if (item.textContent.lenght < count) {
-         return();
-      } else {
-         const.
-      }
-   )
+function sliceTitle(selector, count) {
+   document.querySelectorAll(selector).forEach(item => {
+       item.textContent.trim();
+       if(item.textContent.length < count) {
+           return;
+       } else {
+           const str = item.textContent.slice(0, count + 1) + '...';
+           item.textContent = str;
+       }
+   });
 }
 
-// Modal windows
+sliceTitle('.videos__item-descr', 100);
+
+
 function openModal() {
    modal.style.display = 'block';
 }
 
 function closeModal() {
    modal.style.display = 'none';
+   player.stopVideo();
 }
 
 function bindModal(cards) {
-   card.forEach(item => {
-      item.addEventListener('click', (e) => {
-            // комманда отменяет действия браузера
-            e.preventDefault();
-               openModal();
-      });
+   cards.forEach(item => {
+       item.addEventListener('click', (e) => {
+           e.preventDefault();
+           const id = item.getAttribute('data-url');
+           loadVideo(id);
+           openModal();
+       });
    });
 }
+
 bindModal(videos);
 
 function bindNewModal(cards) {
    cards.addEventListener('click', (e) => {
-      // комманда отменяет действия браузера
-         e.preventDefault();
-         openModal();
+       e.preventDefault();
+       const id = cards.getAttribute('data-url');
+       loadVideo(id);
+       openModal();
    });
 }
 
 modal.addEventListener('click', (e) => {
-   if (!e.target.classList.contains('modal__body')) {
-      closeeModal();
+   if(!e.target.classList.contains('modal__body')) {
+       closeModal();
    }
 });
 
-//взять код отсюда 
-// https://developers.google.com/youtube/iframe_api_reference?hl=ru
-function createVideo {
+function createVideo() {
    var tag = document.createElement('script');
-
    tag.src = "https://www.youtube.com/iframe_api";
    var firstScriptTag = document.getElementsByTagName('script')[0];
    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
    setTimeout(() => {
-      player = new YT.Player('frame', {
-         height: '100%',
-         width: '100%',
-         videoId: 'M7lc1UVf-VE'
+       player = new YT.Player('frame', {
+           height: '100%',
+           width: '100%',
+           videoId: 'M7lc1UVf-VE'
        });
-      }, 300);
-   })
+   }, 300);  
 }
 
 createVideo();
 
 function loadVideo(id) {
-   player.loadVideo
+   player.loadVideoById({'videoId': `${id}`});
 }
-  
-
-
-
-
-
-
